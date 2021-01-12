49a049ca42da55df3ea7c0561c72181795f3b84c
Meckoanja82 <52241577+Meckoanja82@users.noreply.github.com>
2021-01-12 14:16:01 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,8 +1,8 @@
 class minecraft {
-  file {'opt/minecraft':
+  file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'opt/minecraft/server.jar':
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar',
   }
