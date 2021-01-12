35f2205b6aff7ff226b43c8569eaa4b7b1252705
Meckoanja82 <52241577+Meckoanja82@users.noreply.github.com>
2021-01-12 11:01:22 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -8,7 +8,7 @@ class profile::ssh_server {
   }
   ssh_authorized_key {'root@master.puppet.vm':
     ensure => present,
-    user => 'root'
-    type => 'ssh-rsa'
+    user => 'root',
+    type => 'ssh-rsa',
     key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDT9/LKce16g6d2cxIeFt8dZjwI0lff9ip51DVCPpNkEdYOydWVW+RuicqDNxn4WxIpKwMhzEuq6bZBpC+K6OwSvdYoRtCghj8yj47805XSzYJtMWhekgzXW2VOT/idWYy3Z4Xewj5UYSKtVMhf/LZl08vjYwnvTfa8qbb2H06Zbm0U+5M7qH7CmMSXh2v00bQl98Lvd8CUbLSyA/+zY8dinffVFbMJv9ug2/ENqdOD9o3dnPHcb8XuaOtpJrEadc7hT1Vd8gaKNM48RJJPyPcP/QXf/7cUvTp1xvxJSzcGHWdJj0c0ve4M464UDsywXYpyODmCzXCH5SFpFiseoLWP',
 }
