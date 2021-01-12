892d2e7e9c3f975e9573b4b1867fd3ba8a4422e1
Meckoanja82 <52241577+Meckoanja82@users.noreply.github.com>
2021-01-12 11:02:26 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -11,4 +11,5 @@ class profile::ssh_server {
     user => 'root',
     type => 'ssh-rsa',
     key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDT9/LKce16g6d2cxIeFt8dZjwI0lff9ip51DVCPpNkEdYOydWVW+RuicqDNxn4WxIpKwMhzEuq6bZBpC+K6OwSvdYoRtCghj8yj47805XSzYJtMWhekgzXW2VOT/idWYy3Z4Xewj5UYSKtVMhf/LZl08vjYwnvTfa8qbb2H06Zbm0U+5M7qH7CmMSXh2v00bQl98Lvd8CUbLSyA/+zY8dinffVFbMJv9ug2/ENqdOD9o3dnPHcb8XuaOtpJrEadc7hT1Vd8gaKNM48RJJPyPcP/QXf/7cUvTp1xvxJSzcGHWdJj0c0ve4M464UDsywXYpyODmCzXCH5SFpFiseoLWP',
+  }
 }
