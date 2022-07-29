2671038d322242804db9584efc29d61d2c451984
Meckoanja82 <52241577+Meckoanja82@users.noreply.github.com>
2022-07-29 12:54:51 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'This is a fucking readme file!!!!!!!\n',
-    owner => 'root',
-  }
+}
+mode 'master.puppet.vm' {
+  include role::master_server
 }
