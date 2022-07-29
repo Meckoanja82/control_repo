27c65f38f3be988b231ab211546177a97b8e5fbf
Meckoanja82 <52241577+Meckoanja82@users.noreply.github.com>
2022-07-29 11:53:13 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default {
   file {'/root/README':
     ensure => file,
-    content => 'This is a fucking readme file!!!!!!!'
+    content => 'This is a fucking readme file!!!!!!!\n'
+    owner => 'root'
   }
 }
